61b1c25718c5899433809c2e2441339f50ccadd3
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2021-06-16 16:18:52 -0500

    chore: homepage desktop view

--- components/formCta/index.vue
+++ components/formCta/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>This is the form</div>
+  <div class="w-full flex flex-row height_900">
+    <div class="w-1/2 bg-black h-full">
+      <div class="pl-28 pt-28">
+        <h1 class="text-white text-6xl">Get in touch</h1>
+      </div>
+    </div>
+    <div class="w-1/2 h-full image">
+      <h1>Hello world</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+.height_900 {
+  height: 900px;
+}
+
+.image {
+  background-image: url('static/img/home_3.jpeg');
+  background-position: center center;
+  background-size: cover;
+}
+</style>
